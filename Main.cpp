922ef152927921d301599b2c53c0da82640c4590
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2025-09-05 13:59:35 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    double a, b;
+	int 
+
+  
+    cout << "Enter the length of side a: ";
+    cin >> a;
+    cout << "Enter the length of side b: ";
+    cin >> b;
+
+
+    c = (a * a) + (b * b);
+
+    cout << "\nBased on your input, the calculated hypotenuse is: " << c << endl;
+    return 0;
+}
+
+
