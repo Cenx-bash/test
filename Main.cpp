20c3327a7353ea9de348980cd5d9d45f83cf882b
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2025-09-06 19:19:04 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -18,5 +18,26 @@ int main() {
     cout << "\nBased on your input, the calculated hypotenuse is: " << c << endl;
     return 0;
 }
+// fuck C++
 
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+    // Declare variables as float (less precision, smaller memory footprint)
+    float a, b, c;
+
+    cout << "Enter the length of side a: ";
+    cin >> a;
+
+    cout << "Enter the length of side b: ";
+    cin >> b;
+
+    // Calculate hypotenuse using Pythagorean theorem
+    c = sqrt((a * a) + (b * b));
+
+    cout << "\nBased on your input, the calculated hypotenuse is: " << c << endl;
+
+    return 0;
+}
